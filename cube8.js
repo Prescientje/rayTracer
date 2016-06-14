2163cc0c746a7bf8186dc630b04e5756a3a47d61
prescientje <james.kedwards@gmail.com>
2016-06-14 21:18:10 +0200

    normal vector now reflects properly

--- cube8.js
+++ cube8.js
@@ -6,9 +6,8 @@ var gl;
 var pointsArray = [];
 var colorsArray = [];
 
-var normalsArray = [];
-var normPointArray = [];
-var MinMaxXYArray = [];
+var normalsArray = []; //each element is [ vec4normal, vec4point ]
+var minMaxArray = []; //each element is [ minx, maxx, miny, maxy, minz, maxz ]
 
 var vertices = [
         vec4( -3, -3,  0, 1.0 ),
@@ -44,25 +43,60 @@ const up = vec3(0.0, 1.0, 0.0);
 // quad uses first index to set color for face
 
 function quad(a, b, c, d) {
-     pointsArray.push(vertices[a]); 
-     colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
-     pointsArray.push(vertices[b]); 
-     colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
-	 
-     pointsArray.push(vertices[b]); 
-     colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
-     pointsArray.push(vertices[c]); 
-     colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
-	 
-     pointsArray.push(vertices[c]); 
-     colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
-     pointsArray.push(vertices[d]); 
-     colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
+    pointsArray.push(vertices[a]); 
+    colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
+    pointsArray.push(vertices[b]); 
+    colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
+ 
+    pointsArray.push(vertices[b]); 
+    colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
+    pointsArray.push(vertices[c]); 
+    colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
+ 
+    pointsArray.push(vertices[c]); 
+    colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
+    pointsArray.push(vertices[d]); 
+    colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
 
-     pointsArray.push(vertices[d]); 
-     colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
-     pointsArray.push(vertices[a]); 
-     colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
+    pointsArray.push(vertices[d]); 
+    colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
+    pointsArray.push(vertices[a]); 
+    colorsArray.push(vec4( 0.0, 0.0, 0.0, 1.0 ));   // black
+
+
+    var diff1 = subtract(vertices[a],vertices[b]);
+    var diff2 = subtract(vertices[a],vertices[c]);
+    var norm = vec4(cross(diff2, diff1),0.0);
+    normalsArray.push( [norm, a] );
+    
+
+    minMaxArray.push( [Math.min(vertices[a][0],
+		    	        vertices[b][0],
+			        vertices[c][0],
+			        vertices[d][0]),
+    		       Math.max(vertices[a][0],
+		    	        vertices[b][0],
+			        vertices[c][0],
+			        vertices[d][0]),
+                       Math.min(vertices[a][1],
+		    	        vertices[b][1],
+			        vertices[c][1],
+			        vertices[d][1]),
+                       Math.max(vertices[a][1],
+		    	        vertices[b][1],
+			        vertices[c][1],
+			        vertices[d][1]),
+                       Math.min(vertices[a][2],
+		    	        vertices[b][2],
+			        vertices[c][2],
+			        vertices[d][2]),
+                       Math.max(vertices[a][2],
+		    	        vertices[b][2],
+			        vertices[c][2],
+			        vertices[d][2]) ]);
+
+
+     
 }
 
 // Each face determines two triangles
@@ -78,26 +112,47 @@ function colorCube()
 }
 
 function findIntersectionTime(p1, p2, p3, rs, rv) {
-    
-    var diff1 = subtract(p1,p2);
-    var diff2 = subtract(p1,p3);
-    var norm = vec4(cross(diff1, diff2),0.0);
+    var diff1 = subtract(p2,p1);
+    var diff2 = subtract(p3,p1);
+    var norm = vec4(cross(diff2, diff1),0.0);
     var subt = subtract(p1,rs);
     var t = dot(norm,subt)/dot(norm,rv);
     return t;
+}
+
+function findReflectionVector(p1, p2, p3, rv) {
+    var diff1 = subtract(p2,p1);
+    var diff2 = subtract(p3,p2);
+    var norm = vec4(cross(diff1, diff2), 0.0);
+    var rvnorm = normalize(rv, true);
+    var normnorm = normalize(norm, true);
+    return scale(-1,subtract(scale(2*dot(rvnorm,normnorm), normnorm), rvnorm));
 }
 
 function traceRays(rs) {
 
     var rs_new;
     //var rs = vec4(-3.0,-3.0,-1.0, 1.0); //ray position
-    var rv = vec4( 1.0, 0.2, 0.0, 0.0); //ray velocity
+    var rv = vec4( 1.0, 0.2, 0.1, 0.0); //ray velocity
     var rc = 5; //ray "segment count"
     var step = 25;
     var hasIntersected = 10;
 
     
     var t = findIntersectionTime(vertices[2],vertices[3],vertices[6],rs,rv);
+    var p = add(rs, scale(t,rv));
+    var rv_new = findReflectionVector(vertices[2],vertices[3],vertices[6],rv);
+    var rs_end = add(p, scale(t,rv_new));
+	pointsArray.push(rs);
+	pointsArray.push(p);
+	    colorsArray.push(vec4(0,0,0,1));
+	    colorsArray.push(vec4(0,0,0,1));
+	pointsArray.push(p);
+	pointsArray.push(rs_end);
+	    colorsArray.push(vec4(1,0,0,1));
+	    colorsArray.push(vec4(1,0,0,1));
+
+    /*
     for (var i=0; i<step; i++) {
 	rs_new = add(rs, scale(rc/step,rv));
 	pointsArray.push(rs);
@@ -112,7 +167,6 @@ function traceRays(rs) {
         rs = rs_new;
     }
 
-    /*
     var t = findIntersectionTime(vertices[2],vertices[3],vertices[6],rs,rv);
     var rend = add(rs, scale(t, rv));
     alert(rend);
@@ -150,7 +204,8 @@ window.onload = function init() {
     gl.useProgram( program );
     
     colorCube();
-    computeNormalsAndSuch();
+    alert(minMaxArray[0]);
+    //computeNormalsAndSuch();
     traceRays(vec4(-3.0,-3.0,-1.0, 1.0));
     traceRays(vec4(-3.0,-2.0,-2.0, 1.0));
     traceRays(vec4(-3.0, 0.0,-1.0, 1.0));
